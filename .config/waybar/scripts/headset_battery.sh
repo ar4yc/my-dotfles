a48ebf04f17b3806dcee6919de717706807e4c00
Axell <axellpmail@gmail.com>
2026-01-06 14:33:06 +0300

    Update

--- .config/waybar/scripts/headset_battery.sh
+++ .config/waybar/scripts/headset_battery.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-# устройство G435 через UPower
 DEVICE="/org/freedesktop/UPower/devices/headset_dev_40_58_99_3C_BF_1C"
-
-# получаем процент
 percentage=$(upower -i "$DEVICE" | awk '/percentage/ {print $2}' | tr -d '%')
-
-# если нет данных — ставим N/A
 [ -z "$percentage" ] && percentage="N/A"
-
-# вывод для Waybar (только наушники)
 echo "{\"text\": \"🎧 ${percentage}%\", \"tooltip\": \"Headset: ${percentage}%\"}"
